2ef605a9e9199ebe656e511b8ba5d87ceecf5b2c
lacctrDress20 <1490244@senati.pe>
2024-05-14 22:02:54 -0500

    update V

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -20,7 +20,12 @@
           <button class="sidebar-toggle"><i class="fa fa-long-arrow-left"></i></button>
         </div>
           <!-- Log out               -->
-          <div class="list-inline-item logout">                   <a id="logout" href="login.html" class="nav-link">Logout <i class="icon-logout"></i></a></div>
+          <div class="list-inline-item logout">
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <input type="submit" value="Logout">
+            </form>
+          </div>
         </div>
       </div>
     </nav>
